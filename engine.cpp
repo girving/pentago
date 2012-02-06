47a5a9c80ec017173f22d8d11dd46a8253951130
Geoffrey Irving <irving@naml.us>
2012-02-05 16:34:46 -0800

    Add a transposition table
    This seems to work, but doesn't help as much as I'd hoped.  Time to
    think about decent evaluation functions.

--- engine.cpp
+++ engine.cpp
@@ -21,8 +21,11 @@
 using std::max;
 using std::cout;
 using std::endl;
+using std::swap;
 
 namespace {
+
+/**************** Debugging ****************/
 
 // A simple verion of static_assert
 template<bool c> struct assertion_helper {};
@@ -32,6 +35,36 @@ template<> struct assertion_helper<true> { enum {value = 0}; };
 
 // We require 64 bits for now to make interaction with python easier
 static_assert(sizeof(long)==8);
+
+struct python_error {};
+
+#define raise(exn,format,...) \
+  do { \
+    PyErr_Format(PyExc_##exn,format,__VA_ARGS__); \
+    throw python_error(); \
+  } while (0)
+
+#define slow_assert(condition) \
+  if (condition) {} else raise(AssertionError,"file %s, line %d, condition %s",__FILE__,__LINE__,#condition)
+
+inline void check_interrupts() {
+  if (PyErr_CheckSignals())
+    throw python_error();
+}
+
+/***************** Statistics ****************/
+
+#define STAT(...) __VA_ARGS__
+
+uint64_t expanded_nodes;
+uint64_t total_lookups;
+uint64_t successful_lookups;
+
+void clear_stats() {
+  expanded_nodes = total_lookups = successful_lookups = 0;
+}
+
+/***************** Engine ****************/
 
 // Each board is divided into 4 quadrants, and each quadrant is stored
 // in one of the 16-bit quarters of a 64-bit int.  Within a quadrant,
@@ -44,6 +77,18 @@ typedef uint64_t side_t;
 
 // A single quadrant always fits into uint16_t, whether in radix 2 or 3.
 typedef uint16_t quadrant_t;
+
+// The two low bits of the score are the result: loss 0, tie 1, win 2.
+// Above these are 8 bits giving the depth of the result, e.g., >=36 for known and 0 for completely unknown.
+typedef uint16_t score_t;
+
+inline score_t score(int depth, int value) {
+  return depth<<2|value;
+}
+
+inline score_t exact_score(int value) {
+  return score(36,value);
+}
 
 template<int q> inline quadrant_t quadrant(uint64_t state) {
   static_assert(0<=q && q<4);
@@ -96,17 +141,63 @@ template<int s> inline side_t unpack(board_t board) {
                    unpack<s>(quadrant<3>(board)));
 }
 
-struct python_error {};
+inline uint64_t hash(board_t key) {
+  // Invertible hash function from http://www.concentric.net/~ttwang/tech/inthash.htm
+  key = (~key) + (key << 21); // key = (key << 21) - key - 1;
+  key = key ^ (key >> 24);
+  key = (key + (key << 3)) + (key << 8); // key * 265
+  key = key ^ (key >> 14);
+  key = (key + (key << 2)) + (key << 4); // key * 21
+  key = key ^ (key >> 28);
+  key = key + (key << 31);
+  return key;
+}
 
-inline void check_interrupts() {
-  if (PyErr_CheckSignals())
-    throw python_error();
+/* Hashed transposition table.
+ *
+ * Notes:
+ * 1. Collisions are resolved simply: the entry with greater depth wins.
+ * 2. Since our hash is bijective, we store only the high order bits of the hash to detect collisions.
+ */
+const int score_bits = 10;
+const int score_mask = (1<<score_bits)-1;
+int table_bits = 0;
+uint64_t table_mask = 0;
+uint64_t* table = 0;
+
+void init_table(int bits) {
+  if (64-bits+10>64)
+    raise(ValueError,"bits = %d is too small, the high order hash bits won't fit",bits);
+  free(table);
+  table_bits = bits;
+  table_mask = (1<<table_bits)-1;
+  table = (uint64_t*)calloc(1L<<bits,sizeof(uint64_t));
+}
+
+score_t lookup(board_t board) {
+  STAT(total_lookups++);
+  uint64_t h = hash(board);
+  uint64_t entry = table[h&table_mask];
+  if (entry>>score_bits==h>>table_bits) {
+    STAT(successful_lookups++);
+    return entry&score_mask;
+  }
+  return 1;
+}
+
+void store(board_t board, score_t score) {
+  uint64_t h = hash(board);
+  uint64_t& entry = table[h&table_mask];
+  if (entry>>score_bits==h>>table_bits || (entry&score_mask)>>2 <= score>>2)
+    entry = h>>table_bits<<score_bits|score;
 }
 
 // Evaluate the current status of a board, returning one bit for whether each player has 5 in a row.
-int status(board_t board) {
+inline int status(board_t board) {
   const side_t side0 = unpack<0>(board),
                side1 = unpack<1>(board);
+  if ((side0|side1)==0x1ff01ff01ff01ff)
+    return 3; // The board is full, so immediate tie
   const int won0 = won(side0),
             won1 = won(side1);
   return won0|won1<<1;
@@ -116,11 +207,11 @@ int status(board_t board) {
 // static const uint16_t rotations[512][2] = {...};
 #include "gen/rotate.h"
 // static const uint16_t move_offsets[(1<<9)+1] = {...};
-// static const uint16_t move_flat[move_offsets[1<<9]] = {...}; 
+// static const uint16_t move_flat[move_offsets[1<<9]] = {...};
 #include "gen/move.h"
 
 // We declare the move listing code as a huge macro in order to use it in multiple functions while taking advantage of gcc's variable size arrays.
-// To use, declare side0 and side1 variables and call MOVES() to fill a board_t moves[total] array.
+// To use, invoke MOVES(board) to fill a board_t moves[total] array.
 #define QR0(s,q,dir) rotations[quadrant<q>(side##s)][dir]
 #define QR1(s,q) {QR0(s,q,0),QR0(s,q,1)}
 #define QR2(s) {QR1(s,0),QR1(s,1),QR1(s,2),QR1(s,3)}
@@ -139,12 +230,10 @@ int status(board_t board) {
   } \
   const quadrant_t both##i = pack(side0_quad##i,side1_quad##i);
 #define MOVE(q,qr,dir) { \
-  /* printf("sides: %lx %lx\n",(long)side0,(long)side1); */ \
   MOVE_QUAD(q,qr,dir,0) \
   MOVE_QUAD(q,qr,dir,1) \
   MOVE_QUAD(q,qr,dir,2) \
   MOVE_QUAD(q,qr,dir,3) \
-  /* printf("boths: %x %x %x %x\n",both0,both1,both2,both3); */ \
   moves[total##q+8*i+2*qr+dir] = quadrants(both0,both1,both2,both3); \
 }
 #define COLLECT_MOVES(q) \
@@ -155,50 +244,96 @@ int status(board_t board) {
     MOVE(q,2,0) MOVE(q,2,1) \
     MOVE(q,3,0) MOVE(q,3,1) \
   }
-#define MOVES() \
+#define MOVES(board) \
+  /* Unpack sides */ \
+  const side_t side0 = unpack<0>(board), \
+               side1 = unpack<1>(board); \
   /* Rotate all four quadrants left and right in preparation for move generation */ \
   const quadrant_t rotated[2][4][2] = {QR2(0),QR2(1)}; \
   /* Count the number of moves in each quadrant */ \
   const side_t filled = side0|side1; \
   const int total0 = 0; \
   COUNT(0,1) COUNT(1,2) COUNT(2,3) COUNT(3,4) \
-  const int total = total4; \
+  int total = total4; /* Leave mutable to allow in-place pruning of the list */ \
   /* Collect the list of all possible moves.  Note that we repack with the sides */ \
   /* flipped so that it's still player 0's turn. */ \
   board_t moves[total]; \
   COLLECT_MOVES(0) COLLECT_MOVES(1) COLLECT_MOVES(2) COLLECT_MOVES(3)
 
-// Evaluate a position down to a certain game tree depth.  If the depth is exceeded, assume a tie.
-// We maintain the invariant that player 0 is always the player to move.  The result is 1 for a win
-// 0 for tie, -1 for loss.
-int evaluate(int depth, board_t board) {
+// Evaluate position based only on current state and transposition table
+inline score_t quick_evaluate(board_t board) {
   // Is the current position a win, loss, or immediate tie?
-  const side_t side0 = unpack<0>(board),
-               side1 = unpack<1>(board);
-  const int won0 = won(side0),
-            won1 = won(side1);
-  if (won0 || won1 || !depth)
-    return won0-won1;
+  const int st = status(board);
+  if (st)
+    return exact_score(1+(st&1)-(st>>1));
+  // Did we already compute this value?
+  return lookup(board);
+}
 
+// Flip a score and increment its depth by one
+inline score_t lift(score_t sc) {
+  return score(1+(sc>>2),2-(sc&3));
+}
+
+// Same as evaluate, but assume board status and transposition table have supplied no information.
+score_t evaluate_recurse(int depth, board_t board) {
+  STAT(expanded_nodes++);
   // Collect possible moves
-  // const board_t moves[total] = {...};
-  MOVES()
-  if (!total)
-    return 0;
+  // board_t moves[total] = {...};
+  MOVES(board)
 
-  // No move ordering for now
-  int best = -1;
-  for (int i=0;i<total;i++) {
-    int result = -evaluate(depth-1,moves[i]);
-    if (result==1)
-      return 1;
-    best = max(best,result);
+  // Check status and transposition table for each move
+  score_t best = exact_score(0);
+  for (int i=total-1;i>=0;i--) {
+    score_t sc = lift(quick_evaluate(moves[i]));
+    if (sc>>2 >= depth)
+      switch (sc&3) {
+        case 2: return sc; // Win!
+        case 1: best = sc; // Tie: falls through to next case
+        case 0: swap(moves[i],moves[--total]); // Loss or tie: remove from the list of moves
+      }
   }
+
+  // If we're out of moves, we're done
+  if (!total)
+    goto done;
+
+  // Are we out of recursion depth?
+  if (depth==1) {
+    best = score(1,1);
+    goto done;
+  }
+
+  // No move ordering for now, since all the remaining moves are ties as far the transposition table knows
+  for (int i=0;i<total;i++) {
+    score_t sc = lift(evaluate_recurse(depth-1,moves[i]));
+    if ((sc&3)==2)
+      return sc;
+    else if ((best&3)<(sc&3))
+      best = sc;
+  }
+
+  // Store results and finish
+  done:
+  store(board,best);
   check_interrupts();
   return best;
 }
 
-// Python interface
+// Evaluate a position down to a certain game tree depth.  If the depth is exceeded, assume a tie.
+// We maintain the invariant that player 0 is always the player to move.  The result is 1 for a win
+// 0 for tie, -1 for loss.
+score_t evaluate(int depth, board_t board) {
+  // Exit immediately if possible
+  score_t sc = quick_evaluate(board);
+  if (sc>>2 >= depth)
+    return sc;
+
+  // Otherwise, recurse into children
+  return evaluate_recurse(depth,board);
+}
+
+/***************** Python interface ****************/
 
 void check_board(board_t board) {
   #define CHECK(q) \
@@ -215,14 +350,12 @@ PyObject* status_py(PyObject* self, PyObject* args, PyObject* kwargs) {
   if (!PyArg_ParseTupleAndKeywords(args,kwargs,"l",(char**)names,&board,0))
     return 0;
 
-  int result;
   try {
     check_board(board);
-    result = status(board); 
+    return PyInt_FromLong(status(board));
   } catch (...) {
     return 0;
   }
-  return PyInt_FromLong(result);
 }
 
 PyObject* evaluate_py(PyObject* self, PyObject* args, PyObject* kwargs) {
@@ -232,19 +365,16 @@ PyObject* evaluate_py(PyObject* self, PyObject* args, PyObject* kwargs) {
   if (!PyArg_ParseTupleAndKeywords(args,kwargs,"il",(char**)names,&depth,&board,0))
     return 0;
 
-  if (depth<0) {
-    PyErr_Format(PyExc_ValueError,"expected nonnegative depth, got %d",depth);
-    return 0;
-  }
-
-  int result;
   try {
+    if (depth<0)
+      raise(ValueError,"expected nonnegative depth, got %d",depth);
     check_board(board);
-    result = evaluate(depth,board); 
+    if (table_bits<10)
+      raise(AssertionError,"transposition table not initialized: table_bits = %d",table_bits);
+    return PyInt_FromLong(evaluate(depth,board));
   } catch (...) {
     return 0;
   }
-  return PyInt_FromLong(result);
 }
 
 PyObject* moves_py(PyObject* self, PyObject* args, PyObject* kwargs) {
@@ -255,10 +385,8 @@ PyObject* moves_py(PyObject* self, PyObject* args, PyObject* kwargs) {
 
   try {
     check_board(board);
-    const side_t side0 = unpack<0>(board),
-                 side1 = unpack<1>(board);
     // const board_t moves[total] = {...};
-    MOVES()
+    MOVES(board)
     PyObject* tuple = PyTuple_New(total);
     for (int i=0;i<total;i++)
       PyTuple_SET_ITEM(tuple,i,PyInt_FromLong(moves[i]));
@@ -268,10 +396,50 @@ PyObject* moves_py(PyObject* self, PyObject* args, PyObject* kwargs) {
   }
 }
 
+PyObject* init_table_py(PyObject* self, PyObject* args, PyObject* kwargs) {
+  int bits;
+  static const char* names[] = {"bits",0};
+  if (!PyArg_ParseTupleAndKeywords(args,kwargs,"i",(char**)names,&bits,0))
+    return 0;
+
+  try {
+    if (bits<1 || bits>30)
+      raise(ValueError,"expected 1<=bits<=30, got bits = %d",bits);
+    init_table(bits);
+    Py_RETURN_NONE;
+  } catch (...) {
+    return 0;
+  }
+}
+
+PyObject* clear_stats_py(PyObject* self, PyObject* args, PyObject* kwargs) {
+  static const char* names[] = {0};
+  if (!PyArg_ParseTupleAndKeywords(args,kwargs,"",(char**)names,0))
+    return 0;
+  clear_stats();
+  Py_RETURN_NONE;
+}
+
+PyObject* stats_py(PyObject* self, PyObject* args, PyObject* kwargs) {
+  static const char* names[] = {0};
+  if (!PyArg_ParseTupleAndKeywords(args,kwargs,"",(char**)names,0))
+    return 0;
+
+  PyObject* stats = PyDict_New();
+  #define ST(stat) PyDict_SetItemString(stats,#stat,PyInt_FromLong(stat));
+  ST(expanded_nodes)
+  ST(total_lookups)
+  ST(successful_lookups)
+  return stats;
+}
+
 PyMethodDef methods[] = {
-  {"status",  (PyCFunction)status_py,METH_VARARGS|METH_KEYWORDS,"compute the current status of a board"},
-  {"evaluate",(PyCFunction)evaluate_py,METH_VARARGS|METH_KEYWORDS,"evaluate(depth,board) evaluates the given packed Pentago position to the specified depth"},
-  {"moves",   (PyCFunction)moves_py,METH_VARARGS|METH_KEYWORDS,"compute the set of player 0's possible moves starting from a given position"},
+  {"status",     (PyCFunction)status_py,     METH_VARARGS|METH_KEYWORDS,"compute the current status of a board"},
+  {"evaluate",   (PyCFunction)evaluate_py,   METH_VARARGS|METH_KEYWORDS,"evaluate(depth,board) evaluates the given packed Pentago position to the specified depth"},
+  {"moves",      (PyCFunction)moves_py,      METH_VARARGS|METH_KEYWORDS,"compute the set of player 0's possible moves starting from a given position"},
+  {"init_table", (PyCFunction)init_table_py, METH_VARARGS|METH_KEYWORDS,"initialize a transposition table with the given number of bits"},
+  {"clear_stats",(PyCFunction)clear_stats_py,METH_VARARGS|METH_KEYWORDS,"clear statistics"},
+  {"stats",      (PyCFunction)stats_py,      METH_VARARGS|METH_KEYWORDS,"retrieve statistics"},
   {0} // sentinel
 };
 
--- interface.py
+++ interface.py
@@ -35,6 +35,12 @@ def show_board(board):
 def moves(board,turn):
   return [flipto(m,1-turn) for m in engine.moves(flipto(board,turn))]
 
+def lift(score):
+  depth,value = divmod(score,4)
+  if value!=1:
+    assert depth>=36, 'invalid score: depth %d, value %d'%(depth,value)
+  return 4*(depth+1)+2-value
+
 move_pattern = re.compile(r'^\s*([abcdef])([123456])\s*([ul])([lr])\s*([lr])\s*$')
 def parse_move(board,turn,move):
   m = move_pattern.match(move)
@@ -68,38 +74,45 @@ def inv_parse_move(board,turn,next):
   raise ValueError('invalid move')
 
 def move(board,turn,depth):
-  '''Returns (next,result), where next is the board position moved to and result = -1 (loss), 0 (tie), or 1 (win).'''
+  '''Returns (next,score), where next is the board position moved to and score = depth<<2 | 0 (loss), 1 (tie), or 2 (win).'''
   nexts = engine.moves(flipto(board,turn))
   assert nexts
-  best_result = -2
+  options = []
   for next in nexts:
-    result = -engine.evaluate(depth,next)
-    if best_result<result:
-      best_result = result
-      best_next = next
-      if result==1:
+    score = lift(engine.evaluate(depth-1,next))
+    assert score//4>=depth, 'unexpected evaluation: depth %d, value %d, expected depth %d'%(score//4,score&3,depth)
+    if not options or (options[0][0]&3)<(score&3):
+      options = [(score,next)]
+      if (score&3)==2:
         break
-  return flipto(best_next,1-turn),best_result
+    elif (options[0][0]&3)==(score&3):
+      options.append((score,next))
+  score,next = options[0]
+  return flipto(next,1-turn),score
  
 def play(board,turn,sides,depth):
-  print '\n---------------------------------------------------------'
-  print 'board = %d, turn = %d\n\n%s'%(board,turn,show_board(board))
-  s = status(board)
-  if s:
-    print {1:'win for 0',2:'win for 1',3:'simultaneous win (tie)'}[s]
-    return
-  if turn in sides:
-    next,result = move(board,turn,depth)
-    ms = inv_parse_move(board,turn,next)
-    print "move '%s', score %d (%s)"%(ms,result,('win for '+'01'[turn^(result<0)] if result else 'tie'))
-  else:
-    while 1:
-      ms = raw_input('enter move (example: a2 ur l): ')
-      try:
-        next = parse_move(board,turn,ms) 
-      except ValueError:
-        continue
-      except SyntaxError:
-        continue
-      break
-  play(next,1-turn,sides,depth)
+  while 1:
+    print '\n---------------------------------------------------------'
+    print 'board = %d, turn = %d\n\n%s'%(board,turn,show_board(board))
+    s = status(board)
+    if s:
+      print {1:'win for 0',2:'win for 1',3:'simultaneous win (tie)'}[s]
+      return
+    if turn in sides:
+      engine.clear_stats()
+      next,result = move(board,turn,depth)
+      ms = inv_parse_move(board,turn,next)
+      d,value = divmod(result,4)
+      print "move '%s', depth %d, score %d (%s)"%(ms,d,value-1,('win for '+'01'[turn^(value<1)] if value!=1 else 'tie'))
+      print ', '.join('%s = %d'%(k,v) for k,v in engine.stats().items())
+    else:
+      while 1:
+        ms = raw_input('enter move (example: a2 ur l): ')
+        try:
+          next = parse_move(board,turn,ms) 
+        except ValueError:
+          continue
+        except SyntaxError:
+          continue
+        break
+    board,turn = next,1-turn
