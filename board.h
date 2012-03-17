fe2585b958e6e09815897c654e3ed81c16354c2b
Geoffrey Irving <irving@naml.us>
2012-03-17 14:34:05 -0700

    board.h: More comments about the two sides

--- board.h
+++ board.h
@@ -7,6 +7,11 @@ namespace pentago {
 // Each board is divided into 4 quadrants, and each quadrant is stored
 // in one of the 16-bit quarters of a 64-bit int.  Within a quadrant,
 // the state is packed in radix 3, which works since 3**9 < 2**16.
+// The two players are denoted 0 and 1, and the presence of a stone of
+// player k's is denoted by 2**k.  In the interface and other slow code
+// player 0 is usually black, who is assumed to move first.  During
+// tree search, it is always player 0's turn (the sides are swapped
+// after each move).
 typedef uint64_t board_t;
 
 // A side (i.e., the set of stones occupied by one player) is similarly
