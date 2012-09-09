d0a67c5afa382762daaa95f00fff7098a6c5f84c
Geoffrey Irving <irving@naml.us>
2012-09-08 18:21:01 -0700

    draw-history: Implement custom rasterization in order to scale to 96 core runs

--- analyze.cpp
+++ analyze.cpp
@@ -3,7 +3,8 @@
 #include <pentago/section.h>
 #include <pentago/superscore.h>
 #include <pentago/utility/wall_time.h>
-#include <other/core/array/Array.h>
+#include <other/core/array/Array2d.h>
+#include <other/core/geometry/Box.h>
 #include <other/core/python/module.h>
 #include <other/core/random/Random.h>
 #include <other/core/utility/range.h>
@@ -39,10 +40,27 @@ Array<Vector<wall_time_t,2>> simplify_history(RawArray<const Vector<wall_time_t,
   return merged;
 }
 
+// Rasterize a piece of history data into an rgba image
+void rasterize_history(RawArray<Vector<real,4>,2> image, const Vector<real,2> sizes, RawArray<const Vector<wall_time_t,2>> history, const Box<real> y_range, const Vector<real,4> color) {
+  typedef Vector<real,2> TV;
+  typedef Vector<int,2> IV;
+  auto scales = TV(image.sizes()+1)/sizes;
+  scales.x *= 1e-6;
+  const auto scaled_y_range = scales.y*y_range;
+  for (const auto event : history) {
+    const Box<TV> box(vec(scales.x*event.x.us,scaled_y_range.min),vec(scales.x*event.y.us,scaled_y_range.max));
+    const Box<IV> ibox(clamp_min(IV(floor(box.min)),0),clamp_max(IV(ceil(box.max)),image.sizes()));
+    for (int i : range(ibox.min.x,ibox.max.x))
+      for (int j : range(ibox.min.y,ibox.max.y))
+        image(i,j) += color*Box<TV>::intersect(box,Box<TV>(TV(i,j),TV(i+1,j+1))).volume();
+  }
+}
+
 }
 using namespace pentago;
 
 void wrap_analyze() {
   OTHER_FUNCTION(sample_immediate_endings)
   OTHER_FUNCTION(simplify_history)
+  OTHER_FUNCTION(rasterize_history)
 }
