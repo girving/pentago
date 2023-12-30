dfc5cdc32a71da5eed76b088e63387d6992fdfd1
Geoffrey Irving <irving@naml.us>
2023-12-30 21:48:15 +0000

    Upgrade to svelte 4
    Unfortunately this grows main.js from 24,233 to 24,492 bytes.

--- web/client/rollup.config.js
+++ web/client/rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from 'rollup-plugin-svelte'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 import omt from "@surma/rollup-plugin-off-main-thread"
 import ignore from 'rollup-plugin-ignore'
 import { visualizer } from 'rollup-plugin-visualizer'
