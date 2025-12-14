47c986abcf2144c72b5db7cfc0d6a98210d5093e
Geoffrey Irving <irving@naml.us>
2025-12-14 20:11:43 +0000

    Strip jax-metal, since it's defunct

--- learn/setup.py
+++ learn/setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import platform
 from setuptools import setup
 
 setup(name='pentago',
@@ -14,16 +13,14 @@ setup(name='pentago',
           'aiofiles<24.0.0,>=0.7.0',  # For gcloud-aio-storage
           'chardet<4.0',  # For gcloud-aio
           'dm-haiku>=0.0.3',
-          'flax>=0.10.4',
+          'flax>=0.12.1',
           'gcloud-aio-storage>=9.3.0',
-          'jax==0.5.0',  # Dodging https://github.com/jax-ml/jax/issues/27062
+          'jax>=0.8.1',
           'optax>=0.0.6',
           'pytest>=8.3.5',
           'pytest-asyncio>=0.15.1',
           'requests>=2.25.1',
           'wandb',
-      ] + [
-          'jax-metal>=0.1.1',
-      ] * (platform.system() == 'Darwin'),
+      ],
       packages = [],
 )
